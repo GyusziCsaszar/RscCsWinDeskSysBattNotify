db27e87a7cc9780ea2423dbc0d62a583a92adaec
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2022-12-31 11:16:19 +0100

    Initial commit

---
+++ RscSysBattNotify/RscSysBattNotify/FormMain.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace RscSysBattNotify
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblPowerLineValue = new System.Windows.Forms.Label();
+            this.lblPowerLine = new System.Windows.Forms.Label();
+            this.lblBatteryCharge = new System.Windows.Forms.Label();
+            this.lblBatteryChargeValue = new System.Windows.Forms.Label();
+            this.lblBatteryLife = new System.Windows.Forms.Label();
+            this.lblBatteryLifeValue = new System.Windows.Forms.Label();
+            this.lblBatteryFullLifetime = new System.Windows.Forms.Label();
+            this.lblBatteryFullLifetimeValue = new System.Windows.Forms.Label();
+            this.lblBatteryLifeRemaining = new System.Windows.Forms.Label();
+            this.lblBatteryLifeRemainingValue = new System.Windows.Forms.Label();
+            this.tmrRefresh = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(247, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(26, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblPowerLineValue
+            // 
+            this.lblPowerLineValue.AutoSize = true;
+            this.lblPowerLineValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPowerLineValue.ForeColor = System.Drawing.Color.White;
+            this.lblPowerLineValue.Location = new System.Drawing.Point(170, 15);
+            this.lblPowerLineValue.Name = "lblPowerLineValue";
+            this.lblPowerLineValue.Size = new System.Drawing.Size(31, 17);
+            this.lblPowerLineValue.TabIndex = 2;
+            this.lblPowerLineValue.Text = "N/A";
+            // 
+            // lblPowerLine
+            // 
+            this.lblPowerLine.AutoSize = true;
+            this.lblPowerLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblPowerLine.ForeColor = System.Drawing.Color.White;
+            this.lblPowerLine.Location = new System.Drawing.Point(82, 15);
+            this.lblPowerLine.Name = "lblPowerLine";
+            this.lblPowerLine.Size = new System.Drawing.Size(82, 17);
+            this.lblPowerLine.TabIndex = 3;
+            this.lblPowerLine.Text = "Power Line:";
+            // 
+            // lblBatteryCharge
+            // 
+            this.lblBatteryCharge.AutoSize = true;
+            this.lblBatteryCharge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryCharge.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryCharge.Location = new System.Drawing.Point(57, 41);
+            this.lblBatteryCharge.Name = "lblBatteryCharge";
+            this.lblBatteryCharge.Size = new System.Drawing.Size(107, 17);
+            this.lblBatteryCharge.TabIndex = 4;
+            this.lblBatteryCharge.Text = "Battery Charge:";
+            // 
+            // lblBatteryChargeValue
+            // 
+            this.lblBatteryChargeValue.AutoSize = true;
+            this.lblBatteryChargeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryChargeValue.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryChargeValue.Location = new System.Drawing.Point(170, 41);
+            this.lblBatteryChargeValue.Name = "lblBatteryChargeValue";
+            this.lblBatteryChargeValue.Size = new System.Drawing.Size(31, 17);
+            this.lblBatteryChargeValue.TabIndex = 5;
+            this.lblBatteryChargeValue.Text = "N/A";
+            // 
+            // lblBatteryLife
+            // 
+            this.lblBatteryLife.AutoSize = true;
+            this.lblBatteryLife.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryLife.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryLife.Location = new System.Drawing.Point(70, 69);
+            this.lblBatteryLife.Name = "lblBatteryLife";
+            this.lblBatteryLife.Size = new System.Drawing.Size(94, 20);
+            this.lblBatteryLife.TabIndex = 6;
+            this.lblBatteryLife.Text = "Battery Life:";
+            // 
+            // lblBatteryLifeValue
+            // 
+            this.lblBatteryLifeValue.AutoSize = true;
+            this.lblBatteryLifeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryLifeValue.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryLifeValue.Location = new System.Drawing.Point(169, 69);
+            this.lblBatteryLifeValue.Name = "lblBatteryLifeValue";
+            this.lblBatteryLifeValue.Size = new System.Drawing.Size(38, 20);
+            this.lblBatteryLifeValue.TabIndex = 7;
+            this.lblBatteryLifeValue.Text = "N/A";
+            // 
+            // lblBatteryFullLifetime
+            // 
+            this.lblBatteryFullLifetime.AutoSize = true;
+            this.lblBatteryFullLifetime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryFullLifetime.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryFullLifetime.Location = new System.Drawing.Point(28, 127);
+            this.lblBatteryFullLifetime.Name = "lblBatteryFullLifetime";
+            this.lblBatteryFullLifetime.Size = new System.Drawing.Size(136, 17);
+            this.lblBatteryFullLifetime.TabIndex = 8;
+            this.lblBatteryFullLifetime.Text = "Battery Full Lifetime:";
+            // 
+            // lblBatteryFullLifetimeValue
+            // 
+            this.lblBatteryFullLifetimeValue.AutoSize = true;
+            this.lblBatteryFullLifetimeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryFullLifetimeValue.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryFullLifetimeValue.Location = new System.Drawing.Point(170, 127);
+            this.lblBatteryFullLifetimeValue.Name = "lblBatteryFullLifetimeValue";
+            this.lblBatteryFullLifetimeValue.Size = new System.Drawing.Size(31, 17);
+            this.lblBatteryFullLifetimeValue.TabIndex = 9;
+            this.lblBatteryFullLifetimeValue.Text = "N/A";
+            // 
+            // lblBatteryLifeRemaining
+            // 
+            this.lblBatteryLifeRemaining.AutoSize = true;
+            this.lblBatteryLifeRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryLifeRemaining.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryLifeRemaining.Location = new System.Drawing.Point(9, 100);
+            this.lblBatteryLifeRemaining.Name = "lblBatteryLifeRemaining";
+            this.lblBatteryLifeRemaining.Size = new System.Drawing.Size(155, 17);
+            this.lblBatteryLifeRemaining.TabIndex = 10;
+            this.lblBatteryLifeRemaining.Text = "Battery Life Remaining:";
+            // 
+            // lblBatteryLifeRemainingValue
+            // 
+            this.lblBatteryLifeRemainingValue.AutoSize = true;
+            this.lblBatteryLifeRemainingValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBatteryLifeRemainingValue.ForeColor = System.Drawing.Color.White;
+            this.lblBatteryLifeRemainingValue.Location = new System.Drawing.Point(170, 100);
+            this.lblBatteryLifeRemainingValue.Name = "lblBatteryLifeRemainingValue";
+            this.lblBatteryLifeRemainingValue.Size = new System.Drawing.Size(31, 17);
+            this.lblBatteryLifeRemainingValue.TabIndex = 11;
+            this.lblBatteryLifeRemainingValue.Text = "N/A";
+            // 
+            // tmrRefresh
+            // 
+            this.tmrRefresh.Enabled = true;
+            this.tmrRefresh.Interval = 60000;
+            this.tmrRefresh.Tick += new System.EventHandler(this.tmrRefresh_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 126);
+            this.Controls.Add(this.lblBatteryLifeRemainingValue);
+            this.Controls.Add(this.lblBatteryLifeRemaining);
+            this.Controls.Add(this.lblBatteryFullLifetimeValue);
+            this.Controls.Add(this.lblBatteryFullLifetime);
+            this.Controls.Add(this.lblBatteryLifeValue);
+            this.Controls.Add(this.lblBatteryLife);
+            this.Controls.Add(this.lblBatteryChargeValue);
+            this.Controls.Add(this.lblBatteryCharge);
+            this.Controls.Add(this.lblPowerLine);
+            this.Controls.Add(this.lblPowerLineValue);
+            this.Controls.Add(this.btnClose);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.ShowIcon = false;
+            this.Text = "FormMain";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblPowerLineValue;
+        private System.Windows.Forms.Label lblPowerLine;
+        private System.Windows.Forms.Label lblBatteryCharge;
+        private System.Windows.Forms.Label lblBatteryChargeValue;
+        private System.Windows.Forms.Label lblBatteryLife;
+        private System.Windows.Forms.Label lblBatteryLifeValue;
+        private System.Windows.Forms.Label lblBatteryFullLifetime;
+        private System.Windows.Forms.Label lblBatteryFullLifetimeValue;
+        private System.Windows.Forms.Label lblBatteryLifeRemaining;
+        private System.Windows.Forms.Label lblBatteryLifeRemainingValue;
+        private System.Windows.Forms.Timer tmrRefresh;
+    }
+}
+
---
+++ RscSysBattNotify/RscSysBattNotify/FormMain.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Reflection;
+
+namespace RscSysBattNotify
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
+            Rectangle rect = Screen.FromControl(this).WorkingArea; // Bounds;
+            this.Left = rect.Left + (rect.Width - (this.Width + 5));
+            this.Top = rect.Top + (rect.Height - (this.Height + 5));
+
+            RefreshPowerStatus();
+        }
+
+        private void RefreshPowerStatus()
+        {
+            lblPowerLineValue.Text = GetPowerStatusValueAsString("PowerLineStatus");
+            lblBatteryChargeValue.Text = GetPowerStatusValueAsString("BatteryChargeStatus");
+
+            lblBatteryLifeValue.Text = GetPowerStatusValueAsString("BatteryLifePercent");
+            int iBattPerc = Int32.Parse(lblBatteryLifeValue.Text.Substring(0, 3).Trim());
+            if (iBattPerc >= 90)
+            {
+                lblBatteryLifeValue.ForeColor = Color.Green;
+            }
+            else if (iBattPerc <= 10)
+            {
+                lblBatteryLifeValue.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblBatteryLifeValue.ForeColor = Color.Orange;
+            }
+
+            lblBatteryFullLifetimeValue.Text = GetPowerStatusValueAsString("BatteryFullLifetime");
+            lblBatteryLifeRemainingValue.Text = GetPowerStatusValueAsString("BatteryLifeRemaining");
+        }
+
+        private string GetPowerStatusValueAsString(string sName)
+        {
+            Type t = typeof(System.Windows.Forms.PowerStatus);
+            PropertyInfo[] pi = t.GetProperties();
+            for (int i = 0; i < pi.Length; i++)
+            {
+                if (pi[i].Name == sName)
+                {
+                    object oVal = pi[i].GetValue(SystemInformation.PowerStatus, null);
+
+                    if (sName == "BatteryLifePercent")
+                    {
+                        float fVal = (float) oVal;
+                        fVal = fVal * 100;
+                        int iVal = (int) fVal;
+                        return iVal.ToString() + " %"; 
+                    }
+                    else if (sName == "BatteryLifeRemaining")
+                    {
+                        int iVal = (int) oVal;
+
+                        if (iVal < 0) return "N/A";
+
+                        int iHour = iVal / 3600;
+                        iVal = iVal - (iHour * 3600);
+
+                        int iMin = iVal / 60;
+                        iVal = iVal - (iMin * 60);
+
+                        int iSec = iVal;
+
+                        DateTime dt = new DateTime(1, 1, 1, iHour, iMin, iSec);
+                        return dt.ToShortTimeString();
+                    }
+                    else if (sName == "BatteryChargeStatus")
+                    {
+                        string sVal = oVal.ToString();
+                        if (sVal == "0") return "Discharging";
+                        return sVal;
+                    }
+                    else
+                    {
+                        return oVal.ToString();
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tmrRefresh_Tick(object sender, EventArgs e)
+        {
+            RefreshPowerStatus();
+        }
+    }
+}
